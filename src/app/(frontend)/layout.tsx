3b3c80c32da98d5a2bbd955e2229195c6fabcecf
zhakasov-bm <zhakasov.bm2003@gmail.com>
2025-07-22 01:58:24 +0500

    update metadata

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -10,8 +10,9 @@ import { Providers } from './_components/providers/provider'
 import Footer from './_components/Footer/Footer'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description:
+    'Зачисление денег для подготовки рекламных кампаний на площадках Яндекс Маркет, Яндекс Директ, Google ADW, Тик Ток, ВКонтакте, Meta (Facebook, Instagram) на выгодных условиях для юридических лиц и ИП.',
+  title: 'Пополнение рекламных кабинетов',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
