1e022fdad3736598b99f5b7be1386e5b32e5ca3d
zhakasov-bm <zhakasov.bm2003@gmail.com>
2025-07-25 20:46:30 +0500

    add dark clients logo

--- src/app/(frontend)/_components/ClientsBlock.tsx
+++ src/app/(frontend)/_components/ClientsBlock.tsx
@@ -1,7 +1,14 @@
+'use client'
+
 import { Page } from '@/payload-types'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
 export default function ClientsBlock({ page }: { page: Page }) {
+  const { resolvedTheme } = useTheme()
+
+  if (!resolvedTheme) return null
+
   return (
     <section className="container-class pb-8">
       {(page.layout ?? []).map((block, id) => {
@@ -12,12 +19,19 @@ export default function ClientsBlock({ page }: { page: Page }) {
 
               <div className="grid grid-cols-2 md:grid-cols-5 md:gap-4">
                 {block.logos?.map((item, idx) => {
+                  const logoToUse =
+                    resolvedTheme === 'dark' &&
+                    typeof item.logoDark === 'object' &&
+                    item.logoDark?.url
+                      ? item.logoDark
+                      : item.logo
+
                   return (
                     <div key={idx} className="p-2 w-full flex items-center justify-center">
-                      {typeof item.logo === 'object' && item.logo?.url && (
+                      {typeof logoToUse === 'object' && logoToUse?.url && (
                         <Image
-                          src={item.logo.url}
-                          alt={item.logo.alt || 'Icon'}
+                          src={logoToUse.url}
+                          alt={logoToUse.alt || 'Icon'}
                           width={120}
                           height={120}
                           className="object-contain"
